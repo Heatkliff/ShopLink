88dfa395c50da800a445a5d2a3eaffbfa584c3f9
Mykyta Koriak <mykyta.koriak@onix-systems.com>
2022-01-11 15:35:01 +0200

    Added model Category

--- shop/products/models.py
+++ shop/products/models.py
@@ -8,11 +8,21 @@ class ProductPhoto(models.Model):
         return f"{self.image.path}"
 
 
+class Category(models.Model):
+    title = models.CharField(max_length=50, blank=True)
+    description = models.TextField(max_length=500)
+    photos = models.ForeignKey(ProductPhoto, on_delete=models.DO_NOTHING)
+
+    def __str__(self):
+        return f"{self.title}"
+
+
 class Product(models.Model):
     title = models.CharField(max_length=100, blank=True)
     description = models.TextField(max_length=500)
     photos = models.ForeignKey(ProductPhoto, on_delete=models.DO_NOTHING)
     price = models.FloatField(blank=False, default=0.0)
+    category = models.ForeignKey(Category, on_delete=models.DO_NOTHING, blank=True, null=True)
 
     def __str__(self):
         return f"{self.title} : {self.price}"
